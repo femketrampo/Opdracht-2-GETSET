b3d8b1389d9bfd98b3506659dc190d2bc3e3c72c
femketrampo <76632910+femketrampo@users.noreply.github.com>
2022-03-22 09:52:18 +0100

    Add files via upload

---
+++ Opdracht-2-GETSET/Opdracht-2-GETSET/Breuk.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opdracht_2
+{
+    class Breuk
+    {
+        private double teller;
+        private double noemer;
+
+        public double Teller
+        {
+            get { return teller; }
+            set { teller = value; }
+        }
+
+        public double Noemer
+        {
+            get { return noemer; }
+            set { noemer = value; }
+        }
+
+        public Breuk()
+        {
+            teller = 4;
+            noemer = 4;
+        }
+        public Breuk(double Teller, double Noemer)
+        {
+            teller = Teller;
+            noemer = Noemer;
+        }
+    }
+}
---
+++ Opdracht-2-GETSET/Opdracht-2-GETSET/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Opdracht_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Breuk breuk = new Breuk();
+
+            Opvraging(breuk);
+
+            Console.WriteLine("Het quotient is: {0}", Quotient(breuk));
+            Console.ReadLine();
+        }
+        private static void Opvraging(Breuk breuk)
+        {
+            Console.Write("Geef de teller in: ");
+            breuk.Teller = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Geef de noemer in: ");
+            breuk.Teller = Convert.ToDouble(Console.ReadLine());
+        }
+        private static double Quotient(Breuk breuk)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            return breuk.Teller / breuk.Noemer;
+        }
+    }
+}
